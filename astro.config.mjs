c8af4069146a8d0072c2111c31bf242b7f101f17
emmanuel <almerux@gmail.com>
2026-07-19 13:11:17 -0600

    chore(json): add astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://axvolestudio.com',
+
+  integrations: [sitemap()]
 });
